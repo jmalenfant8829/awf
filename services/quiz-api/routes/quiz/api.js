6deb066ed163d648d1cc09d6ade93646f5a7b9f7
Michael Gordon <michael.gordon.1993@gmail.com>
2021-03-19 13:02:56 -0400

    API route endpoints declared

---
+++ services/quiz-api/routes/quiz/api.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+var config = require('../../config/database');
+require('../config/passport')(passport);
+var express = require('express');
+var router = express.Router();
+
+
+
+//Fetches a list of all available quizzes, could be given query string
+//specifying name
+router.get('/quiz', function (req, res){
+
+});
+
+//Creates a new quiz with the supplied information. 
+//Expected Data: Name, Question Data 
+router.post('/quiz', function(req, res){
+
+});
+
+//Fetches question and other data for a single quiz
+router.get('/quiz/:quizId', function (req, res){
+
+});
+
+//Updates a quiz with the specified ID using the supplied information. 
+//Expected Data: Name, Question Data (JSON) 
+router.patch('/quiz/:quizId', function(req, res){
+
+});
+
+//Deletes the quiz with the specified ID
+router.delete('/quiz/:quizId', function(req, res){
+
+});
+
+//Submits a response for a quiz. 
+//Expected Data: Answer Data (JSON)
+router.post('/quiz/:quizId', function(req, res){
+
+});
+
+//Like a quiz if it is not already liked, unlike it if it is already liked
+router.get('/quiz/like/:quizId', function(req, res){
+
+});
