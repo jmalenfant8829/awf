6324dd97f9e42da11b8b57154d071e31230e6b35
Julien Malenfant <Jmalenfant8829@conestogac.on.ca>
2021-04-23 13:48:48 -0400

    use explicit backend url

--- web/src/api/authApi.js
+++ web/src/api/authApi.js
@@ -4,10 +4,12 @@ const postHeaders = {
   'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
 }
 
+const backendURL = process.env.REACT_APP_BACKEND_URL
+
 export function registerUser (data) {
-  return axios.post('/api/auth/register', data, postHeaders)
+  return axios.post(backendURL + '/api/auth/register', data, postHeaders)
 }
 
 export function loginUser (data) {
-  return axios.post('/api/auth/login', data, postHeaders)
+  return axios.post(backendURL + '/api/auth/login', data, postHeaders)
 }
--- web/src/api/quizApi.js
+++ web/src/api/quizApi.js
@@ -4,14 +4,16 @@ import axios from 'axios'
 //   'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
 // }
 
+const backendURL = process.env.REACT_APP_BACKEND_URL
+
 const postJSONHeaders = {
   'Content-Type': 'application/json'
 }
 
 export function getAllQuizzes () {
-  return axios.get('/api/quiz/')
+  return axios.get(backendURL + '/api/quiz/')
 }
 
 export function createQuiz (data) {
-  return axios.post('/api/quiz', data, postJSONHeaders)
+  return axios.post(backendURL + '/api/quiz', data, postJSONHeaders)
 }
